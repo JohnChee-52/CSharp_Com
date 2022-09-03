0697eec7946ea3a144c2378544ced4902fc95a06
John Chee <John Chee@DESKTOP-HG6CPK9>
2022-09-03 09:29:32 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RS485_Port {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        [STAThread]
+        static void Main() {
+            //Thread workerThread = new Thread(new ThreadStart(Data_Rx));
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form_SerialCom1());
+        }
+
+ 
+    }
+}
