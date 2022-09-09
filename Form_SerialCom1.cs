e5515be16a372d08db9d74e7f40d3dd4f50bd906
Johnny Chee <john.chee@biosensedx.com.sg>
2022-09-09 21:22:18 +0800

    Read 1 line from serial port,  show on textbox

--- Form_SerialCom1.cs
+++ Form_SerialCom1.cs
@@ -13,13 +13,13 @@ using System.Windows.Forms;
 namespace RS485_Port {
     
     public partial class Form_SerialCom1 : Form {
-        public static string str_DataRxed_from_SerPort = "fake rxed data";
+        public static string str_RxData = "fake rxed data";
         private static bool bF_SerPort_Ready = false;
         //cc-------------------------------------------------------------------------------------------------------------
         public Form_SerialCom1() {
             InitializeComponent();
             populate_PortNames();
-            str_DataRxed_from_SerPort = "No data yet";
+            str_RxData = "No data yet";
             //Thread SerialPort_Thread = new Thread(new ThreadStart(serialPort1DataReceived));
             //SerialPort_Thread.Start();  // Start secondary thread  
 
@@ -47,6 +47,7 @@ namespace RS485_Port {
 
                 SerPort.Open();
                 progressBar_PortAvailable.Value = 100;
+                Thread.Sleep(200);
                 textBox_Status.Text = SerPort.PortName + " is now opened.";
                 for (long i = 0; i < 1000000; i++) {
                     ;
@@ -84,14 +85,14 @@ namespace RS485_Port {
         //--------------------------------------------------------------------------------------------------------------
         private void SerPort_read_n_show() {
             if (bF_SerPort_Ready) {
-                str_DataRxed_from_SerPort = SerPort.ReadLine();
-                textBox_RxData.Text += str_DataRxed_from_SerPort + "\r\n";
+                str_RxData = SerPort.ReadLine();
+                textBox_RxData.Text += str_RxData + "\r\n";
             }
         }
         //--------------------------------------------------------------------------------------------------------------
-        public void serialPort1DataReceived(object sender, SerialDataReceivedEventArgs e) {
+        public void serialPort1DataRxed(object sender, SerialDataReceivedEventArgs e) {
             try {
-                str_DataRxed_from_SerPort = SerPort.ReadLine();
+                str_RxData = SerPort.ReadLine();
 
 
                 Invoke(new EventHandler(putData));
@@ -102,14 +103,8 @@ namespace RS485_Port {
         }
         //--------------------------------------------------------------------------------------------------------------
         private void putData(object sender, EventArgs e) {
-            textBox_RxData.Text += str_DataRxed_from_SerPort;
+            textBox_RxData.Text += str_RxData;
         }
         //--------------------------------------------------------------------------------------------------------------
-        //public static void Data_Rx() {
-        //    str_DataRxed_from_SerialPort = serialPort1.ReadLine();
-        //    textBox_RxData.Text += str_DataRxed_from_SerialPort + "\r\n";
-
-        //}
-        //--------------------------------------------------------------------------------------------------------------
-    }
+     }
 }
